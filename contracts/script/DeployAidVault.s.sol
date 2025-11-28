5d370d1431b27f1edbdac847e8e59001e86b4c28
morelucks <luckykamshak@gmail.com>
2025-11-28 05:51:54 +0100

    Add deployment script for AidVault (Destination contract)

---
+++ contracts/script/DeployAidVault.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {AidVault} from "../src/AidVault.sol";
+
+contract DeployAidVault is Script {
+    function run() public {
+        address callbackProxy = vm.envAddress("DESTINATION_CALLBACK_PROXY_ADDR");
+        
+        vm.startBroadcast();
+        
+        AidVault vault = new AidVault(callbackProxy);
+        
+        console.log("AidVault deployed at:", address(vault));
+        console.log("Callback proxy:", callbackProxy);
+        
+        vm.stopBroadcast();
+    }
+}
+
