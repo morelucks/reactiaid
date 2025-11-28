d8ac38876fb2b6d7b7ac8d413670654bba8786fd
morelucks <luckykamshak@gmail.com>
2025-11-28 05:51:03 +0100

    Add deployment script for DisasterOracle (Origin contract)

---
+++ contracts/script/DeployDisasterOracle.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {DisasterOracle} from "../src/DisasterOracle.sol";
+
+contract DeployDisasterOracle is Script {
+    function run() public {
+        vm.startBroadcast();
+        
+        DisasterOracle oracle = new DisasterOracle();
+        
+        console.log("DisasterOracle deployed at:", address(oracle));
+        
+        vm.stopBroadcast();
+    }
+}
+
